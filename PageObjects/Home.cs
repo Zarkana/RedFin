ce4fa2b6a98a841b8ba0aa91af83624ee3ef3546
Joe Campbell <jsc940@gmail.com>
2019-03-12 11:30:26 -0700

    add directories and basepage object

---
+++ PageObjects/Home.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedFin.PageObjects
+{
+    class Home : BasePage
+    {
+        private IWebDriver driver;
+
+        private Home(IWebDriver driver) : base(driver)
+        {
+            this.driver = driver;
+        }
+
+        public static Home Initialize(IWebDriver driver)
+        {
+            return new PageObjects.Home(driver);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,23 @@ using System.Threading.Tasks;
 
 namespace RedFin
 {
+
+    /**     
+     *Automate the following test scenario:
+     * Property Search: Using Redfin (https://www.redfin.com), write an automated test that checks whether the property search returned the correct results.
+     * 
+     * Test Details:
+     * Use your city to perform the property search.
+     * Use at least 3 filters to perform the search.
+     * Verify that the results returned matches the search criteria.
+     * 
+     * Instructions:
+     * Use GitHub for your repository.  When complete, please send the link to your GitHub repo.
+     * You may use any programming language, testing framework, and library to complete the assignment.
+     * You must use a design pattern such as Page Object Model.
+     * Feel free to ask questions regarding the assignment. 
+     **/
+
     class Program
     {
         static void Main(string[] args)
